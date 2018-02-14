15d07ace7b1d8a514d5fedd2bd3a3f0b8de8c448
Tsiry MANANKASINA <Manankas@users.noreply.github.com>
2018-02-14 16:41:05 +0300

    add LambdaDemo file

---
+++ LambdaDemo.java
@@ -0,0 +1,20 @@
+package trav_maivana7;
+
+public class LambdaDemo {
+	
+	@FunctionalInterface
+	interface Instrument {                  //une interface fonctionnelle n’est rien d’autre qu’une interface avec une seule méthode abstraite. 
+		boolean jouer(String morceau);  
+		
+	}
+
+	public static void main(String[] args) {
+	
+		//instanciation suivi de l'implémentation du fonction 
+		Instrument i = (morceau)-> {System.out.println("Jouer : " +morceau+" à la guitare");return true;};
+		
+		boolean res = i.jouer("vive le vent");
+		System.out.println(res);
+	}
+
+}
