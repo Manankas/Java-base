8f7ee03160fc2c731bb0921072a5bad32e524d93
tsiriniavo <tsirymanankasina@gmail.com>
2018-02-18 14:38:34 +0300

    suppr

--- IOC/src/dao/IDataFromBase.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-public interface IDataFromBase {
-     public int getTemperature();
-}
--- IOC/src/service/ICalculateur.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public interface ICalculateur {
-	public int convertTemperature();
-}
